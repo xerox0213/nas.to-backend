b47ccd6ce19d1cb01686217fc055b5325f85db46
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-24 21:44:12 +0200

    fix(articles): make cover_image_url column nullable

--- database/migrations/2025_04_22_190649_create_articles_table.php
+++ database/migrations/2025_04_22_190649_create_articles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,7 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('cover_image_url');
+            $table->string('cover_image_url')->nullable();
             $table->string('title');
             $table->text('content');
             $table->timestamps();
