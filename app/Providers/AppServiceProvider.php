bb29a531c32e2cf102d03648d8fd40a134de49fd
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-24 22:26:25 +0200

    chore(injection): register ImageLocalUploader

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\ImageUploader;
+use App\Services\LocalImageUploader;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(ImageUploader::class, LocalImageUploader::class);
     }
 
     /**
