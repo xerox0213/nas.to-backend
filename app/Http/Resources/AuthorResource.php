292b97713ae83efce0dc8cbc945fc6afae7258a0
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-01 14:06:56 +0200

    feat: add AuthorResource

---
+++ app/Http/Resources/AuthorResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin User */
+class AuthorResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'avatar_image_url' => $this->avatar_image_url
+        ];
+    }
+}
