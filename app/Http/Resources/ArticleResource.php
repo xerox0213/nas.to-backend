8bb4cfd22036d9a1d3ff00fd30f38874ed2d9d90
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-01 14:07:02 +0200

    feat: add ArticleResource

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Article */
+class ArticleResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'cover_image_url' => $this->cover_image_url,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'author' => new AuthorResource($this->user)
+        ];
+    }
+}
